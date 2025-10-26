a8f9ae718e1015fb0f73c861718de81888ac09cc
aleksandarPaytalov <apaytalov89@gmail.com>
2025-10-26 19:22:34 +0200

    update services with dynamic data

--- src/data.js
+++ src/data.js
@@ -10,3 +10,24 @@ export const navIcons = [
   { id: 2, href: "https://www.twitter.com", iconFab: "fab fa-twitter" },
   { id: 3, href: "https://www.twitter.com", iconFab: "fab fa-squarespace" },
 ];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    titleText: "endless hiking",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
